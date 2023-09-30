3e6779eb6a76dd39b671154a1cd608ae18a9cfed
DanylLeStyle <102415006+DanylLeStyle@users.noreply.github.com>
2023-09-30 12:19:12 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -13,43 +13,47 @@ window.addEventListener("mousemove", function (e){
     // cursorOutline.style.top = `${posY}px`;
 }); */
 
+const cursor = document.querySelector('[data-cursor-dot]'); 
+let mouseX = 0;
+let mouseY = 0;
+const maxVelocity = 20;
 
-const cursor = document.querySelector('[data-cursor-dot]'); // Sélectionner l'élément du curseur personnalisé
-    let mouseX = 0;
-    let mouseY = 0;
-    let cursorX = 0;
-    let cursorY = 0;
-    const maxVelocity = 20; // Définir la vitesse maximale en pixels par frame
+function updateCursor() {
+  const dx = mouseX - (cursor.offsetWidth / 2);
+  const dy = mouseY - (cursor.offsetHeight / 2);
 
-    function updateCursor() {
-      const dx = mouseX - cursorX;
-      const dy = mouseY - cursorY;
+  const distance = Math.sqrt(dx * dx + dy * dy);
+  const smoothingFactor = 0.2;
+  const velocity = Math.min(distance * smoothingFactor, maxVelocity);
 
-      // Calculer la distance entre le curseur et la position de la souris
-      const distance = Math.sqrt(dx * dx + dy * dy);
+  cursor.style.left = mouseX - (cursor.offsetWidth / 2) + "px";
+  cursor.style.top = mouseY - (cursor.offsetHeight / 2) + "px";
 
-      // Calculer la vitesse en fonction de la distance et du smoothingFactor
-      const smoothingFactor = 0.2; // Plus la valeur est grande, moins le lissage est important
-      const velocity = Math.min(distance * smoothingFactor, maxVelocity);
+  requestAnimationFrame(updateCursor);
+}
 
-      // Mettre à jour la position du curseur personnalisé
-      cursorX += (dx / distance) * velocity;
-      cursorY += (dy / distance) * velocity;
-      cursor.style.left = cursorX + "px";
-      cursor.style.top = cursorY + "px";
+document.addEventListener('mousemove', (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
 
-      requestAnimationFrame(updateCursor);
-    }
+  if (cursor.style.display === 'none') {
+    cursor.style.display = 'block';
+  }
 
-    document.addEventListener('mousemove', (e) => {
-      mouseX = e.clientX;
-      mouseY = e.clientY;
-      
-      // Appliquer le délai de suivi en millisecondes (par exemple, 100ms)
-      setTimeout(() => {
-        requestAnimationFrame(updateCursor);
-      }, 100);
-    });
+  clearTimeout(cursorTimeout);
+
+  cursorTimeout = setTimeout(() => {
+    cursor.style.display = 'none';
+  }, 300);
+
+  requestAnimationFrame(updateCursor);
+});
+
+
+
+
+
+
 
 // Gérer l'agrandissement du curseur lorsqu'il survole les boutons et les liens href
 const buttonsAndLinks = document.querySelectorAll('a.boutonproduits, a.logo, ul li a');
@@ -83,4 +87,3 @@ document.addEventListener('mousemove', (e) => {
             });*/
 
 /* Sticky Navbar */
-
